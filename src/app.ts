e092f4ef040f760f0997f1ed7373f02ee5e8d504
Zayn <mzainabbas77@gmail.com>
2023-10-19 23:54:08 +0500

    Init

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('😎Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`🔥Express Server running on port ${PORT}`);
+});
