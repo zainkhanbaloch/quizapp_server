c0579604d4fead061fb8f2ac98539b7fbab1ab8d
Zayn <mzainabbas77@gmail.com>
2023-10-21 00:23:59 +0500

    Random numbers duplication bug fixed

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import  handleGetQuestions  from './request_handler';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("😎Hello World!");
@@ -12,7 +14,7 @@ app.get("/", (req, res) => {
 
 
 
-app.get("/get_questions", handleGetQuestions);
+app.post("/get_questions", handleGetQuestions);
 
 app.listen(PORT, () => {
   console.log(`🔥Express Server running on port ${PORT}`);
--- src/request_handler.ts
+++ src/request_handler.ts
@@ -1,90 +1,3 @@
-// import { Request, Response } from "express";
-// import * as fs from "fs";
-
-// const dataToSend = [
-//     { subject: "computer", questions: "5" },
-//     { subject: "physics", questions: "5" },
-//     { subject: "maths", questions: "5" },
-//   ];
-
-// type get_questions_schema = {
-//   subject: string;
-//   questions: number;
-// };
-
-// function generateRandomNumbers(length, min, max) {
-//   const numbers = [];
-
-//   for (let i = 0; i < length; i++) {
-//     numbers.push(Math.floor(Math.random() * (max - min + 1)) + min);
-//   }
-
-//   return numbers;
-// }
-
-// // console.log(generateRandomNumbers(5,0,10))
-// type Questions = {
-//   prompt: string;
-//   answer: string;
-//   options: string[];
-// };
-
-// type ReadFileType<T> = {
-//   data?: T[];
-//   error?: string;
-// };
-
-// async function readQuestionsFile(
-//   subject: string
-// ): Promise<ReadFileType<Questions>> {
-//   const jsonFilePath = `./${subject}_questions.json`;
-
-//   try {
-//     const data = await fs.promises.readFile(jsonFilePath, "utf8");
-//     const jsonData = JSON.parse(data) as Questions[];
-//     return { data: jsonData };
-//   } catch (err) {
-//     console.error(`Error reading/parsing JSON file: ${err.message}`);
-//     return { error: err.message };
-//   }
-// }
-
-// async function getQuestions(data): Promise<Questions[]> {
-//     const questionPromises = data.map(async (block) => {
-//       const subject: string = block.subject;
-//       const requiredQuestions: number = block.questions;
-//       console.log(subject, requiredQuestions);
-  
-//       const questionsFile = await readQuestionsFile(subject);
-//       if (questionsFile.error) {
-//         console.error(questionsFile.error);
-//         return []; // Return an empty array in case of an error
-//       } else {
-//         const questions = questionsFile.data;
-//         console.log("Parsed questions:", questions);
-  
-//         const randomNumbers: number[] = generateRandomNumbers(requiredQuestions, 0, questions.length - 1);
-//         const generatedQuestions: Questions[] = randomNumbers.map((index) => questions[index]);
-//         return generatedQuestions;
-//       }
-//     });
-  
-//     // Use Promise.all to wait for all questionPromises to resolve
-//     return Promise.all(questionPromises).then((results) => {
-//       return results.flat(); // Flatten the array of arrays
-//     });
-//   }
-  
-
-// async function handleGetQuestions(req: Request, res: Response) {
-//   const required_questions: get_questions_schema[] = req.body;
-//   console.log(req.body)
-//   const response  = await getQuestions(required_questions);
-//   res.status(200).json(response);
-// }
-
-// export default handleGetQuestions;
-
 import { Request, Response } from "express";
 import * as fs from "fs";
 
@@ -100,13 +13,20 @@ type get_questions_schema = {
 };
 
 function generateRandomNumbers(length: number, min: number, max: number): number[] {
-  const numbers: number[] = [];
-
-  for (let i = 0; i < length; i++) {
-    numbers.push(Math.floor(Math.random() * (max - min + 1)) + min);
+  if (max - min + 1 < length) {
+    throw new Error("Cannot generate unique random numbers with the given range and length.");
   }
 
-  return numbers;
+  const uniqueNumbers: Set<number> = new Set();
+  while (uniqueNumbers.size < length) {
+    const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    uniqueNumbers.add(randomNum);
+  }
+
+  // Convert the set to an array for the final result
+  const uniqueNumbersArray = Array.from(uniqueNumbers);
+  console.log(uniqueNumbersArray);
+  return uniqueNumbersArray;
 }
 
 type Questions = {
@@ -139,7 +59,7 @@ async function getQuestions(data: get_questions_schema[]): Promise<Questions[]> 
   const questionPromises = data.map(async (block) => {
     const subject: string = block.subject;
     const requiredQuestions: number = block.questions;
-    console.log(subject, requiredQuestions);
+    //console.log(subject, requiredQuestions);
 
     const questionsFile = await readQuestionsFile(subject);
     if (questionsFile.error) {
@@ -147,7 +67,7 @@ async function getQuestions(data: get_questions_schema[]): Promise<Questions[]> 
       return []; // Return an empty array in case of an error
     } else {
       const questions = questionsFile.data;
-      console.log("Parsed questions:", questions);
+      //console.log("Parsed questions:", questions);
 
       const randomNumbers: number[] = generateRandomNumbers(requiredQuestions, 0, questions.length - 1);
       const generatedQuestions: Questions[] = randomNumbers.map((index) => questions[index]);
@@ -163,7 +83,7 @@ async function getQuestions(data: get_questions_schema[]): Promise<Questions[]> 
 async function handleGetQuestions(req: Request, res: Response): Promise<void> {
   const required_questions: get_questions_schema[] = req.body;
   console.log(`request body is ${req.body}`);
-
+  console.log("a request was received")
   try {
     const response = await getQuestions(required_questions);
     res.status(200).json(response);
